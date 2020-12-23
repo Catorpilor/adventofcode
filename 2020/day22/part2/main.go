981fe18b9ecff2c42ecd1957e48d88ff35769d41
Catorpilor <jiaojiangonghitster@gmail.com>
2020-12-23 14:07:05 +0800

    need to figureout why custome struct as key not working

--- 2020/day22/part2/main.go
+++ 2020/day22/part2/main.go
@@ -33,7 +33,8 @@ func main() {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 	p1, p2 := group[0], group[1]
-	_, ans := run(p1, p2, true)
+	_, ans := play(p1, p2)
+	// _, ans := run(p1, p2, true)
 	fmt.Println(ans)
 	// fmt.Println(len(p1) + len(p2))
 	// store := make(map[int]map[Round]bool)
@@ -68,7 +69,6 @@ func main() {
 	// }
 	// fmt.Println(ans)
 }
-
 func run(deck1, deck2 []int, rec bool) (int, int) {
 	seen := map[string]struct{}{}
 
@@ -123,66 +123,42 @@ func toString(a []int) string {
 	return sb.String()
 }
 
-func play(store map[int]map[Round]bool, p1, p2 []int, gr, round int) (int, bool) {
-	if store[gr] == nil {
-		store[gr] = make(map[Round]bool)
-	}
-	fmt.Printf("-- Round %d (Game %d) --\n", round, gr)
-	fmt.Printf("Player 1' deck: %v\n", p1)
-	fmt.Printf("Player 2' deck: %v\n", p2)
-	// fmt.Printf("--debug store[%d]: %v---\n", gr, store[gr])
-	k1, k2 := toString(p1), toString(p2)
-	cur := Round{k1, k2}
-	// rule 1
-	if round > 1 {
-		if store[gr][cur] {
-			fmt.Println("rule 1 matched, play1 wins")
-			fmt.Printf("Play1 wins round %d of game %d!\n", round, gr)
-			return 0, true
+func play(p1, p2 []int) (int, int) {
+	seen := make(map[string]bool)
+	var winner int
+	for len(p1) > 0 && len(p2) > 0 {
+		winner = 1
+		ck := fmt.Sprint(p1, p2)
+		if seen[ck] {
+			break
+		}
+		seen[ck] = true
+		t1, t2 := p1[0], p2[0]
+		p1 = p1[1:]
+		p2 = p2[1:]
+		if t2 > t1 {
+			winner = 2
+		}
+		if len(p1) >= t1 && len(p2) >= t2 {
+			np1, np2 := make([]int, t1), make([]int, t2)
+			copy(np1, p1[:t1])
+			copy(np2, p2[:t2])
+			winner, _ = play(np1, np2)
+		}
+		if winner == 1 {
+			p1 = append(p1, t1, t2)
+		} else {
+			p2 = append(p2, t2, t1)
 		}
 	}
-	store[gr][cur] = true
-	t1, t2 := p1[0], p2[0]
-	p1 = p1[1:]
-	p2 = p2[1:]
-	fmt.Printf("Player 1' plays: %d\n", t1)
-	fmt.Printf("Player 2' plays: %d\n", t2)
-	if len(p1) >= t1 && len(p2) >= t2 {
-		np1, np2 := make([]int, t1), make([]int, t2)
-		copy(np1, p1[:t1])
-		copy(np2, p2[:t2])
-		// sub-game
-		localRound := 1
-		for len(np1) != 0 && len(np2) != 0 {
-			res, ok := play(store, np1, np2, gr+1, localRound)
-			if ok {
-				np2 = nil
-				continue
-			}
-			t1, t2 := np1[0], np2[0]
-			np1 = np1[1:]
-			np2 = np2[1:]
-			if res != 0 {
-				np2 = append(np2, t2, t1)
-				fmt.Printf("Play2 wins round %d of game %d!\n", round, gr+1)
-			} else {
-				np1 = append(np1, t1, t2)
-				fmt.Printf("Play1 wins round %d of game %d!\n", round, gr+1)
-			}
-			localRound++
-		}
-		if len(np1) == 0 {
-			fmt.Printf("...anyway, back to game %d.\nPlay2 wins round %d of game %d!\n", gr, round, gr+1)
-			return 1, false
-		}
-		fmt.Printf("...anyway, back to game %d.\nPlay1 wins round %d of game %d!\n", gr, round, gr+1)
-		return 0, false
-	} else {
-		if t1 < t2 {
-			fmt.Printf("Play2 wins round %d of game %d!\n", round, gr)
-			return 1, false
-		}
-		fmt.Printf("Play1 wins round %d of game %d!\n", round, gr)
-		return 0, false
+	res := p1
+	if winner == 2 {
+		res = p2
 	}
+	n := len(res)
+	var ans int
+	for i := n - 1; i >= 0; i-- {
+		ans += res[i] * (n - i)
+	}
+	return winner, ans
 }
