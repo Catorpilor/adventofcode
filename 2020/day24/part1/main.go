13b9e5055062f55d87c29ecabf2ad2086a579157
Catorpilor <jiaojiangonghitster@gmail.com>
2020-12-25 18:35:37 +0800

    local

--- 2020/day24/part1/main.go
+++ 2020/day24/part1/main.go
@@ -21,10 +21,10 @@ func main() {
 	st := pos{x: 0, y: 0, flip: false}
 	store[st.String()] = &st
 	dirs := map[string]pos{
-		"e":  pos{x: 1, y: 0},
+		"e":  pos{x: 2, y: 0},
 		"se": pos{x: 1, y: -1},
 		"sw": pos{x: -1, y: -1},
-		"w":  pos{x: -1, y: 0},
+		"w":  pos{x: -2, y: 0},
 		"nw": pos{x: -1, y: 1},
 		"ne": pos{x: 1, y: 1},
 	}
@@ -33,7 +33,7 @@ func main() {
 		fmt.Printf("processing txt:%s\n", txt)
 		n := len(txt)
 		var i int
-		cur := st
+		cur := *(store["0,0"])
 		for i < n {
 			var dir string
 			if txt[i] != 'e' && txt[i] != 'w' {
@@ -45,6 +45,9 @@ func main() {
 			}
 			fmt.Printf("direction: %s\n, cur: %#v\n", dir, cur)
 			np := pos{x: cur.x + dirs[dir].x, y: cur.y + dirs[dir].y, count: 1}
+			// if np.x == 0 && np.y == 0 {
+			// 	panic("go back to where we start")
+			// }
 			if op, exists := store[np.String()]; exists {
 				op.x = np.x
 				op.y = np.y
@@ -64,8 +67,9 @@ func main() {
 	}
 	var ans int
 	for _, v := range store {
-		if v.flip {
+		if v.count&1 != 0 {
 			ans++
+			fmt.Printf("tile %s is black\n", v)
 		}
 	}
 	fmt.Println(ans)
