0f6c4144db9a8849e8df56cc4ec774853b9de255
Catorpilor <jiaojiangonghitster@gmail.com>
2022-12-09 04:01:28 -0800

    fix: solve day9 part1

--- 2022/day8/part2/main.go
+++ 2022/day8/part2/main.go
@@ -33,7 +33,7 @@ func main() {
 	fmt.Println(res)
 }
 
-type pos struct{
+type pos struct {
 	left, top, right, bottom int
 }
 
@@ -50,30 +50,73 @@ func max(a, b int) int {
 
 func calc(matrix [][]int, m, n int) int {
 	var res int
-	for i:=0; i<m; i++ {
-		for j:=0; j<n; j++ {
-			if i== 0 || j == 0 || i == m-1 || j== m-1 {
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 || j == 0 || i == m-1 || j == m-1 {
 				continue
 			}
-			var left, top, right, bottom  int
-			for d := i-1; d >= 0; d-- {
+			var left, top, right, bottom int
+			for d := i - 1; d >= 0; d-- {
 				top++
-				if matrix[d][j] >= matrix[i][j] {break}
+				if matrix[d][j] >= matrix[i][j] {
+					break
+				}
 			}
-			for d := j+1; d<n; d++ {
+			for d := j + 1; d < n; d++ {
 				right++
-				if matrix[i][d] >= matrix[i][j] {break}
+				if matrix[i][d] >= matrix[i][j] {
+					break
+				}
 			}
-			for d := i+1; d < m; d++ {
+			for d := i + 1; d < m; d++ {
 				bottom++
-				if matrix[d][j] >= matrix[i][j] {break}
+				if matrix[d][j] >= matrix[i][j] {
+					break
+				}
 			}
-			for d := j-1; d>=0; d-- {
+			for d := j - 1; d >= 0; d-- {
 				left++
-				if matrix[i][d] >= matrix[i][j] {break}
+				if matrix[i][d] >= matrix[i][j] {
+					break
+				}
 			}
 			res = max(res, left*top*right*bottom)
 		}
+	}
+	return res
+}
+
+
+func calc2(matrix [][]int, m, n int) int {
+	var res int
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			score := bfs(matrix, i, j)
+			res = max(res, score)
+		}
+	}
+	return res
+}
+
+func bfs(matrix [][]int, x, y int) int {
+	res := 1
+	dirs := []int{-1, 0, 1, 0, -1}
+	m := len(matrix)
+	n := len(matrix[0])
+	for i := 0; i < 4; i++ {
+		dx, dy := dirs[i], dirs[i+1]
+		visiable := 0
+		for x < m-1 {
+			for y < n-1 {
+				visiable++
+				if matrix[x+dx][y+dy] >= matrix[x][y] {
+					break
+				}
+				x += dx
+				y += dy
+			}
+		}
+		res *= visiable
 	}
 	return res
 }
