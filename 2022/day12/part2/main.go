01d28218dab0e81aa06d6b21f5705ef9d8dee667
Catorpilor <jiaojiangonghitster@gmail.com>
2022-12-14 05:28:29 -0800

    solve day12 part2

--- 2022/day12/part2/main.go
+++ 2022/day12/part2/main.go
@@ -7,109 +7,117 @@ import (
 	"os"
 )
 
-
-type coord struct{x, y int}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func Day12() int {
-	reader := bufio.NewReader(os.Stdin)
-	grid := make([][]byte, 0)
-	for {
-		var line string
-		_, err := fmt.Fscanf(reader, "%s\n", &line)
-		if err != nil {
-			break
-		}
-		grid = append(grid, []byte(line))
-	}
-
-	var (
-		starts []coord
-		end    coord
-	)
-
-	for i := range grid {
-		for j := range grid[i] {
-			if grid[i][j] == 'S' { // or just grid[i][j] == 'S' for task 1
-				starts = append(starts, coord{i, j})
-			} else if grid[i][j] == 'E' {
-				end = coord{i, j}
-			}
-		}
-	}
-
-	minSteps := math.MaxInt
-
-	// to remove E from grid
-	grid[end.x][end.y] = 'z'
-	for _, start := range starts {
-		// to remove possible 'S' from grid
-		grid[start.x][start.y] = 'a'
-		curSteps := bfs(grid, start, end)
-		minSteps = min(minSteps, curSteps)
-	}
-
-	return minSteps
-}
-
-func bfs(grid [][]byte, start, end coord) int {
-	seen := make(map[coord]bool)
-	seen[start] = true
-	queue := []coord{start}
-	steps := 0
-	var dirs = []coord{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
-	found := false
-
-	// simple BFS
-L:
-	for len(queue) > 0 {
-		k := len(queue)
-		for i := 0; i < k; i++ {
-			cur := queue[0]
-			queue = queue[1:]
-
-			if cur == end {
-				found = true
-				break L
-			}
-
-			for _, dir := range dirs {
-				newCoord := coord{cur.x + dir.x, cur.y + dir.y}
-				// isInvalid?
-				if newCoord.x < 0 || newCoord.x >= len(grid) || newCoord.y < 0 || newCoord.y >= len(grid[0]) {
-					continue
-				}
-				// isSeen?
-				if seen[newCoord] {
-					continue
-				}
-				// isTooHigh?
-				isNewCoordGreater := grid[newCoord.x][newCoord.y] > grid[cur.x][cur.y]
-				if isNewCoordGreater && grid[newCoord.x][newCoord.y]-grid[cur.x][cur.y] > 1 {
-					continue
-				}
-				seen[newCoord] = true
-				queue = append(queue, newCoord)
-			}
-		}
-		steps++
-	}
-
-	if !found {
-		fmt.Println(start, "to", end, "not fund")
-		return math.MaxInt
-	}
-
-	return steps
-}
-
-
 func main() {
-	fmt.Println(Day12())
+	scanner := bufio.NewScanner(os.Stdin)
+	var matrix [][]byte
+	var row int
+	var starts []coord
+	var end coord
+	for scanner.Scan() {
+		txt := scanner.Text()
+		if txt == "" {
+			continue
+		}
+		curRow := make([]byte, 0, len(txt))
+		for i := range txt {
+			b := txt[i]
+			if txt[i] == 'a' || txt[i] == 'S' {
+				starts = append(starts, coord{row, i})
+				b = 'a'
+			} else if txt[i] == 'E' {
+				end = coord{row, i}
+				b = 'z'
+			}
+			curRow = append(curRow, b)
+		}
+		matrix = append(matrix, curRow)
+		row++
+		// fmt.Println(scanner.Text()) // Println will add back the final '\n'
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	// fmt.Printf("start(%d, %d), end(%d, %d), m:%d, n:%d\n", sx, sy, ex, ey, len(matrix), len(matrix[0]))
+	ret := math.MaxInt
+	for _, s := range starts {
+		q := &queue{}
+		q.Push(item{0, s})
+		res := bfs(matrix, s, end, q)
+		// fmt.Printf("calculate start: (%d,%d), end(%d, %d) got %d\n", s.x, s.y, end.x, end.y, res)
+		if res < ret {
+			ret = res
+		}
+	}
+	fmt.Println(ret)
+}
+
+type coord struct{ x, y int }
+
+func (c coord) String() string {
+	return fmt.Sprintf("%d-%d", c.x, c.y)
+}
+
+type item struct {
+	depth int
+	c     coord
+}
+
+type queue struct {
+	items []item
+}
+
+func (q *queue) Push(o item) {
+	q.items = append(q.items, o)
+}
+
+func (q *queue) Front() item {
+	return q.items[0]
+}
+
+func (q *queue) Pop() {
+	if len(q.items) > 0 {
+		q.items = q.items[1:]
+	}
+}
+
+func (q *queue) IsEmpty() bool {
+	return len(q.items) == 0
+}
+
+func bfs(matrix [][]byte, start, end coord, q *queue) int {
+	dirs := []int{0, 1, 0, -1, 0}
+	m := len(matrix)
+	n := len(matrix[0])
+	ret := math.MaxInt
+	seen := map[coord]bool{}
+	for !q.IsEmpty() {
+		it := q.Front()
+		q.Pop()
+		ori := matrix[it.c.x][it.c.y]
+		if it.c.x == end.x && it.c.y == end.y {
+			// fmt.Println(it.depth)
+			return it.depth
+		}
+		if seen[it.c]{
+			continue
+		}
+		// fmt.Printf("cur (%s) depth: %d, coord: (%d, %d),visited: %t\n", string(ori), it.depth, it.c.x, it.c.y, visited[it.c.String()])
+		// if visited[it.c.String()] {
+		// 	continue
+		// }
+		// fmt.Printf("cur (%s) depth: %d, coord: (%d, %d)\n", string(ori), it.depth,  it.c.x, it.c.y)
+		// visited[it.c.String()] = true
+		seen[it.c] = true
+		for i := 0; i < 4; i++ {
+			nx, ny := it.c.x+dirs[i], it.c.y+dirs[i+1]
+			if nx < 0 || ny < 0 || nx >= m || ny >= n {
+				continue
+			}
+			nc := coord{nx, ny}
+			if int(matrix[nx][ny]) <= int(ori)+1 {
+				q.Push(item{it.depth + 1, nc})
+			}
+		}
+	}
+	return ret
 }
