8d18193420817bfac9168b79f6e952312608f328
Catorpilor <jiaojiangonghitster@gmail.com>
2023-01-03 14:46:30 +0800

    fix: update old file

---
+++ 2021/day8/part2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	var res int
+	for scanner.Scan() {
+		segs := strings.Split(scanner.Text(), " | ")
+
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+}
